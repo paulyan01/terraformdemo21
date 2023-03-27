f85262df35ba4c613014bf11ea237f016f10970a
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-03-27 19:07:50 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -7,10 +7,10 @@ variable "bucket_name" {
   description = "A list of unique names for the GCP storage buckets."
   type        = list(string)
   default     = [
-    "my-test-bucket-latest8999",
-    "mytestbucket8888",
-    "montrealcollege-927218-for-demo",
-    "bucket-full-of-happiness"
+    "CODEDEPRESSION",
+    "TESTBUCKETS88",
+    "TERRARFORM-demo",
+    "bucket-full-of-DEPRESSION"
 
   ]
 }
